3045453da76995510c92da6e6b69004919868024
Shuja Khalid <shujakhalid97@gmail.com>
2018-05-02 09:38:02 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 
 #!/bin/sh
 
-# Install conda
-bash Anaconda3-5.0.1-Linux-x86_64.sh -yes
-export PATH="/home/%USER_NAME%/anaconda3/bin":$PATH # Where %USER_NAME% is your user name! 
-source ~/.bashrc
+# # Install conda
+# bash Anaconda3-5.0.1-Linux-x86_64.sh -yes
+# export PATH="/home/%USER_NAME%/anaconda3/bin":$PATH # Where %USER_NAME% is your user name! 
+# source ~/.bashrc
 
-# Create a virtual environment
-conda create -n "venv" python=3.6 # Where "venv" is your virtual environment
-source activate venv # Where "venv" is your virtual environment
+# # Create a virtual environment
+# conda create -n "venv" python=3.6 # Where "venv" is your virtual environment
+# source activate venv # Where "venv" is your virtual environment
 
 # Setup nvidia and cuda scripts
 sudo add-apt-repository ppa:graphics-drivers/ppa
@@ -16,25 +16,21 @@ sudo apt-get update
 sudo apt-get install build-essential
 sudo apt-get install python-dev python3-dev python-virtualenv
 sudo apt-get install nvidia-384
-wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_375.26_linux-run
-sudo ./cuda_8.0.61_375.26_linux-run
-export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+# wget 'https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda_9.0.176_384.81_linux-run'
+# sudo ./cuda_9.0.176_384.81_linux-run
+# export PATH=/usr/local/cuda-9.0/bin${PATH:+:${PATH}}
+# export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
 # Download CUDA and CUDAnn
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn6_6.0.21-1%2Bcuda8.0_amd64.deb
-wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn6-dev_6.0.21-1%2Bcuda8.0_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-sudo dpkg -i libcudnn6_6.0.21-1+cuda8.0_amd64.deb
-sudo dpkg -i libcudnn6-dev_6.0.21-1+cuda8.0_amd64.deb
+wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7-dev_7.0.4.31-1+cuda9.0_amd64.deb
+sudo dpkg -i libcudnn7_7.0.4.31-1+cuda9.0_amd64.deb
 sudo apt-get update
-sudo apt-get install cuda=8.0.61-1
-sudo apt-get install libcudnn6-dev
+sudo apt-get install cuda=9.0-1
+sudo apt-get install libcudnn7-dev
 
 # Add folders to PATH
-export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export PATH=/usr/local/cuda-9.0/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
 # Note: Remember to reboot computer after completion!
 
